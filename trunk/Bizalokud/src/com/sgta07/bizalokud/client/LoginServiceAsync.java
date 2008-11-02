cf20601bd168d6b1746e75814027f40fb50ac5ef
Blizarazu <Blizarazu@bf7d0088-a907-11dd-8184-3158da6531e3>
2008-11-02 18:32:12 +0000

    Delete

--- trunk/Bizalokud/src/com/sgta07/bizalokud/client/LoginServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package com.sgta07.bizalokud.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface LoginServiceAsync {
-	
-	public void login(String user, String pass, AsyncCallback<Boolean> callback);
-}
