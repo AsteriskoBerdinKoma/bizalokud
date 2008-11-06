ce5aa1c8f63f31719ad3de9eba77db4a4feafc88
Blizarazu <Blizarazu@bf7d0088-a907-11dd-8184-3158da6531e3>
2008-11-06 20:00:55 +0000

    Login modulu berria

---
+++ trunk/Bizalokud/src/com/sgta07/bizalokud/login/client/LoginServiceAsync.java
@@ -0,0 +1,8 @@
+package com.sgta07.bizalokud.login.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface LoginServiceAsync {
+	
+	public void login(String user, String pass, AsyncCallback<Boolean> callback);
+}
