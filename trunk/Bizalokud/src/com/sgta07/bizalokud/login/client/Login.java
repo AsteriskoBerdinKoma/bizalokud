19ce79d31c14759fe76f46d0d1fedde78a7f32f6
Blizarazu <Blizarazu@bf7d0088-a907-11dd-8184-3158da6531e3>
2008-11-06 22:09:11 +0000

    Login moduluan aldaketa txikiak

--- trunk/Bizalokud/src/com/sgta07/bizalokud/login/client/Login.java
+++ trunk/Bizalokud/src/com/sgta07/bizalokud/login/client/Login.java
@@ -34,39 +34,23 @@ public class Login implements EntryPoint {
 		formPanel.setBorder(false);
 
 		// add some fields
-		FieldSet fieldSet = new FieldSet("Sartu zure erabiltzailea eta pasahitza");
+		FieldSet fieldSet = new FieldSet(
+				"Sartu zure erabiltzailea eta pasahitza");
 		final TextField erabiltzaileaTextField = new TextField("Erabiltzailea",
 				"user", 150);
 		erabiltzaileaTextField.setVtype(VType.ALPHANUM);
-		erabiltzaileaTextField.setVtypeText("Erabiltzaile izenak bakarrik hizkiak, zenbakiak eta _ eduki ditzake");
-		/*erabiltzaileaTextField.setValidator(new Validator() {
-			public boolean validate(String value) throws ValidationException {
-				System.out.println("balidatzen");
-				if (erabiltzaileaTextField.getText().isEmpty())
-					throw new ValidationException(
-							"Erabiltzaile izena sartu behar duzu");
-				else
-					return true;
-			}
-		});*/
+		erabiltzaileaTextField
+				.setVtypeText("Erabiltzaile izenak bakarrik hizkiak, zenbakiak eta _ eduki ditzake");
 		erabiltzaileaTextField.setValidationEvent(false);
-		
+
 		final TextField pasahitzaTextField = new TextField("Pasahitza", "pass",
 				150);
 		pasahitzaTextField.setPassword(true);
-		/*pasahitzaTextField.setValidator(new Validator() {
-			public boolean validate(String value) throws ValidationException {
-				System.out.println("balidatzen");
-				if (erabiltzaileaTextField.getText().isEmpty())
-					throw new ValidationException("Pasahitza sartu behar duzu");
-				else
-					return true;
-			}
-		});*/
 		pasahitzaTextField.setVtype(VType.ALPHANUM);
-		pasahitzaTextField.setVtypeText("Pasahitzak bakarrik hizkiak, zenbakiak eta _ eduki ditzake");
+		pasahitzaTextField
+				.setVtypeText("Pasahitzak bakarrik hizkiak, zenbakiak eta _ eduki ditzake");
 		pasahitzaTextField.setValidationEvent(false);
-		
+
 		fieldSet.add(erabiltzaileaTextField);
 		fieldSet.add(pasahitzaTextField);
 
@@ -75,63 +59,79 @@ public class Login implements EntryPoint {
 		final Button sartuBtn = new Button("Sartu",
 				new ButtonListenerAdapter() {
 					public void onClick(Button button, EventObject e) {
-						
-						if(erabiltzaileaTextField.validate() &&
-						pasahitzaTextField.validate()){
-			
-						MessageBox.show(new MessageBoxConfig() {
-							{
-								setTitle("Login");
-								setMsg("Login egiten, mesedez itxaron...");
-								setProgressText("Login egiten...");
-								setWidth(300);
-								setWait(true);
-								setWaitConfig(new WaitConfig() {
+						if (!erabiltzaileaTextField.getText().isEmpty()
+								&& !pasahitzaTextField.getText().isEmpty()) {
+							// Bi testu kutxak balidatzeko emaitzak aldagaietan
+							// gorde behar dira
+							boolean validErab = erabiltzaileaTextField
+									.validate();
+							boolean validPasa = pasahitzaTextField.validate();
+							if (validErab && validPasa) {
+
+								MessageBox.show(new MessageBoxConfig() {
 									{
-										setInterval(200);
-									}
-								});
-							}
-						});
-						
-						LoginService.Util.getInstance().login(
-								erabiltzaileaTextField.getText(),
-								pasahitzaTextField.getText(),
-								new AsyncCallback<Boolean>() {
-			
-									public void onFailure(Throwable caught) {
-										MessageBox.hide();
-										MessageBox.show(new MessageBoxConfig() {
+										setTitle("Login");
+										setMsg("Login egiten, mesedez itxaron...");
+										setProgressText("Login egiten...");
+										setWidth(300);
+										setWait(true);
+										setWaitConfig(new WaitConfig() {
 											{
-												setTitle("Errorea komunikazioan");
-												setMsg("Ezin izan da zerbitzariarekin komunikazioa ezarri.");
-												setButtons(MessageBox.OK);
-												setIconCls(MessageBox.ERROR);
+												setInterval(200);
 											}
 										});
-										System.out
-										.println(caught.getMessage());
-									}
-			
-									public void onSuccess(Boolean result) {
-										MessageBox.hide();
-										if (result.booleanValue())
-											System.out
-													.println("Sartu zaitezke");
-										else
-											MessageBox
-													.show(new MessageBoxConfig() {
-														{
-															setTitle("Login okerra");
-															setMsg("Erabiltzaile izena edo pasahitza okerra da.");
-															setButtons(MessageBox.OK);
-															setIconCls(MessageBox.ERROR);
-														}
-													});
 									}
 								});
-						}
-						
+
+								LoginService.Util.getInstance().login(
+										erabiltzaileaTextField.getText(),
+										pasahitzaTextField.getText(),
+										new AsyncCallback<Boolean>() {
+
+											public void onFailure(
+													Throwable caught) {
+												MessageBox.hide();
+												MessageBox
+														.show(new MessageBoxConfig() {
+															{
+																setTitle("Errorea komunikazioan");
+																setMsg("Ezin izan da zerbitzariarekin komunikazioa ezarri.");
+																setButtons(MessageBox.OK);
+																setIconCls(MessageBox.ERROR);
+															}
+														});
+												System.out.println(caught
+														.getMessage());
+											}
+
+											public void onSuccess(Boolean result) {
+												MessageBox.hide();
+												if (result.booleanValue())
+													System.out
+															.println("Sartu zaitezke");
+												else
+													MessageBox
+															.show(new MessageBoxConfig() {
+																{
+																	setTitle("Login okerra");
+																	setMsg("Erabiltzaile izena edo pasahitza okerra da.");
+																	setButtons(MessageBox.OK);
+																	setIconCls(MessageBox.ERROR);
+																}
+															});
+											}
+										});
+							}
+
+						} else
+							MessageBox.show(new MessageBoxConfig() {
+								{
+									setTitle("Login okerra");
+									setMsg("Erabiltzaile izena edo pasahitza ez duzu sartu.");
+									setButtons(MessageBox.OK);
+									setIconCls(MessageBox.ERROR);
+								}
+							});
 					}
 				});
 		formPanel.addButton(sartuBtn);
